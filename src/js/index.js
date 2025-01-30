21d1d79c1ff69472a541d00795b759a935916f49
Bryanow <bryan.ricardo7@gmail.com>
2025-01-30 19:05:31 -0300

    creating a rotation cicle on the functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,40 @@
 
-//OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo 
-//cartao da lista
-// - passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
 const btnAvancar = document.getElementById('btn-avancar');
 const btnVoltar = document.getElementById('btn-voltar');
 const cartoes = document.querySelectorAll('.cartao');
 // definindo contador
 let cartaoAtual = 0;
 
-//  - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
 btnAvancar.addEventListener('click', function() {
-    if(cartaoAtual === cartoes.length - 1) return;
 
-    const cartaoSelecionado = document.querySelector('.selecionado')
-    cartaoSelecionado.classList.remove('selecionado')
+    hide_selected_card();
 
     //- passo 3 - fazer aparecer o próximo cartão da lista
-    cartaoAtual ++;
-    cartoes[cartaoAtual].classList.add('selecionado');
+    if (cartaoAtual === cartoes.length - 1) {
+        cartaoAtual = 0;
+    } else {
+        cartaoAtual ++;
+    }
+    showSelectedCard(cartaoAtual);
 
 });
 
-
-/*
-OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão 
-anterior da lista
-  - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
-  - passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
-  - passo 3 - fazer aparecer o cartão anterior da lista
-  - passo 4 - buscar o cartão que esta selecionado e esconder
-*/
 btnVoltar.addEventListener('click', function() {
-    if (cartaoAtual === 0) return;
+    hide_selected_card();
 
-    const cartaoSelecionado = document.querySelector('.selecionado')
-    cartaoSelecionado.classList.remove('selecionado')
-
-    cartaoAtual --;
-    cartoes[cartaoAtual].classList.add('selecionado');
+    if (cartaoAtual === 0) {
+        cartaoAtual = cartoes.length - 1;
+    } else {
+        cartaoAtual --;
+    }
+    showSelectedCard(cartaoAtual);
 });
+
+function showSelectedCard(cartaoAtual) {
+    cartoes[cartaoAtual].classList.add('selecionado');
+}
+
+function hide_selected_card() {
+    const cartaoSelecionado = document.querySelector('.selecionado');
+    cartaoSelecionado.classList.remove('selecionado');
+}
