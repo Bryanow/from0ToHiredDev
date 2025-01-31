0e686e7c6ae0d48fca8735d4e765e4ba26d0a3bf
Bryan <91998706+Bryanow@users.noreply.github.com>
2025-01-31 00:55:10 -0300

    Revert "creating feature to put card on 'defense mode'"

--- src/js/index.js
+++ src/js/index.js
@@ -5,18 +5,6 @@ const cartoes = document.querySelectorAll('.cartao');
 // definindo contador
 let cartaoAtual = 0;
 
-cartoes.forEach(cartao => {
-    cartao.addEventListener('click', function () {
-        const cartaVirada = cartao.querySelector('.carta-virada');
-
-        //virar o cartão
-        cartao.classList.toggle('virar');
-        cartaVirada.classList.toggle('mostrar-fundo-carta');
-
-        const descricao = cartao.querySelector('.descricao');
-        descricao.classList.toggle('esconder')
-    });
-});
 
 btnAvancar.addEventListener('click', function() {
     hide_selected_card();
@@ -39,8 +27,6 @@ btnVoltar.addEventListener('click', function() {
     }
     showSelectedCard(cartaoAtual);
 });
-
-
 
 function showSelectedCard(cartaoAtual) {
     cartoes[cartaoAtual].classList.add('selecionado');
